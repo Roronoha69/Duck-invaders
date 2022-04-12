85bca0e233cd33f593e726e89a4a4f69f7b911a6
Sanzen Sekai ⚔️ <88967908+Roronoha69@users.noreply.github.com>
2022-04-12 11:04:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class Player {
     this.opacity = 1
 
     const image = new Image()
-    image.src = './img/spaceship.png'
+    image.src = './img/spaceship1.png'
     image.onload = () => {
       const scale = 0.15
       this.image = image
